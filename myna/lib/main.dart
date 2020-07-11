645b1ff7571e664ccf0c67d18adf3281a1f6a8e3
Nikhil <2017csb1091@iitrpr.ac.in>
2020-07-11 20:07:06 +0530

    basic running app

--- myna/lib/main.dart
+++ myna/lib/main.dart
@@ -1,31 +1,22 @@
 import 'package:flutter/material.dart';
-import './redux/initialState.dart';
-import './redux/AppState.dart';
-import './redux/reducer.dart';
 import './screens/HomePage/HomePage.dart';
-import './screens/ResultList/ResultList.dart';
 
 void main() {
-  runApp(MyApp(
-    store: store,
-    title: "Myna",
-  ));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({this.store, this.title});
-
   @override
   Widget build(BuildContext context) {
-    return StoreProvider(
-        store: this.store,
-        child: MaterialApp(
-          title: this.title,
-          initialRoute: '/',
-          routes: {
-            '/': (context) => HomePage(),
-            '/resultList': (context) => ResultList(),
-          },
-        ));
+    return MaterialApp(title: "Myna", initialRoute: '/', routes: {
+      '/': (context) => HomePage(),
+      // '/resultList': (context) => ResultList(),
+      // '/login':(context)=>
+      // '/register':()=>
+      // '/chats':()=>
+      // '/chatThread':()=>
+      // '/resultDetail':()=>
+      // '/recordMode':()=>
+    });
   }
 }

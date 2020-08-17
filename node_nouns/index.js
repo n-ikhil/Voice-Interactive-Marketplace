c7b95127f495fa2b173c52a96cd15df5474ecd79
gskumar77c <gskumar77c@gmail.com>
2020-08-17 16:29:19 +0530

    node nouns extracting

---
+++ node_nouns/index.js
@@ -0,0 +1,25 @@
+var pos = require('pos');
+var chunker = require('pos-chunker');
+
+const extractingNouns = (rvstring) => {
+                
+        const words = new pos.Lexer().lex(rvstring);
+        const tags = new pos.Tagger()
+        .tag(words)
+        .map(function(tag){return tag[0] + '/' + tag[1];})
+        .join(' ');
+        places = chunker.chunk(tags, '[{ tag: NNP }]');
+        const noun_check = tags.split(' ');
+        const final_nouns = [];
+        noun_check.forEach(el => {
+            const wrds = el.split('/');
+            const s1 = wrds[0];
+            const s2 = wrds[1];
+            if(s2.length >= 2 && s2[0]=='N' && s2[1] == 'N') final_nouns.push(s1);
+        });
+
+        return final_nouns;
+}
+
+
+console.log(extractingNouns("best placess to buy fertilizers in visakhapatnam"));

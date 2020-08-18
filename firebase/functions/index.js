fe9434aaaf850984e612ce896c4b4cdbfa1c85e8
Nikhil <2017csb1091@iitrpr.ac.in>
2020-08-18 12:09:31 +0530

    linting changes

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,28 +1,28 @@
-const functions = require('firebase-functions');
-var pos = require('pos');
-var chunker = require('pos-chunker');
+const functions = require("firebase-functions");
+var pos = require("pos");
+var chunker = require("pos-chunker");
 // install the above two packages
 
-
 const extractingNouns = (rvstring) => {
-        const words = new pos.Lexer().lex(rvstring);
-        const tags = new pos.Tagger()
-        .tag(words)
-        .map(function(tag){return tag[0] + '/' + tag[1];})
-        .join(' ');
-        places = chunker.chunk(tags, '[{ tag: NNP }]');
-        const noun_check = tags.split(' ');
-        const final_nouns = [];
-        noun_check.forEach(el => {
-            const wrds = el.split('/');
-            const s1 = wrds[0];
-            const s2 = wrds[1];
-            if(s2.length >= 2 && s2[0]=='N' && s2[1] == 'N') final_nouns.push(s1);
-        });
+  const words = new pos.Lexer().lex(rvstring);
+  const tags = new pos.Tagger()
+    .tag(words)
+    .map((tag) => {
+      return tag[0] + "/" + tag[1];
+    })
+    .join(" ");
+  places = chunker.chunk(tags, "[{ tag: NNP }]");
+  const noun_check = tags.split(" ");
+  const final_nouns = [];
+  noun_check.forEach((el) => {
+    const wrds = el.split("/");
+    const s1 = wrds[0];
+    const s2 = wrds[1];
+    if (s2.length >= 2 && s2[0] === "N" && s2[1] === "N") final_nouns.push(s1);
+  });
 
-        return final_nouns;
-}
-
+  return final_nouns;
+};
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
